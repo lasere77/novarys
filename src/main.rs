32f740e0d0c4ee81296b771b6131f239c4df25f2
lasere77 <colin.mewen@gmail.com>
2026-05-10 15:56:10 +0200

    feat: Reads the input file passed as a parameter to extract the rom

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Read;
+
+const ROM_SIZE: usize = 255;
+const NOP_OPCODE: u8 = 192;
+
+fn  get_rom() -> [u8; 255]
+{
+    let file_path: String = std::env::args().nth(1).expect("Please set the path to the binary file.");
+    let file = std::fs::File::open(file_path).expect("Fail to open the file, Please check the path");
+    let mut reader = std::io::BufReader::new(file);
+    let mut rom: [u8; 255] = [NOP_OPCODE; ROM_SIZE];
+    if reader.read(&mut rom).unwrap() > ROM_SIZE
+    {
+        println!("Warning: Please note that the binary file contains more data than the ROM can hold.");
+    }
+    rom
+}
+
+fn main()
+{
+    let rom: [u8; 255] = get_rom();
+    for byte in rom.iter()
+    {
+        println!("{byte}");
+    }
 }
